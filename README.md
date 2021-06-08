16200654f6dd69be193b5bd9e05a892a33b35440
Nicolas Noé <nicolas.noe@inbo.be>
2021-06-08 14:22:11 +0200

    Use black for formatting

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # odim_h5
 
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
 A simple Python package to access data in [ODIM HDF5](https://www.eumetnet.eu/wp-content/uploads/2019/01/ODIM_H5_v23.pdf) format 
 
 Limitations:
@@ -20,3 +22,9 @@ Limitations:
 ## Run tests
 
     $ pytest
+    $ mypy odim_h5
+
+## Format code
+
+    $ black .
+
--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from distutils.core import setup
 
-setup(name='odim_h5',
-      version='0.1.0',  # Following https://semver.org/
-      description='A Python package to read ODIM HDF5 files',
-      author='Nicolas Noé - INBO',
-      author_email='nicolas.noe@inbo.be',
-      packages=['odim_h5'],
-      python_requires='>3.7.0',
-      install_requires=[  # Installation dependencies, let's keep development dependencies in requirements.txt
-          'h5py>=3.1.0',
-          'pytz'
-      ],
-      classifiers=[
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-      ]
+setup(
+    name="odim_h5",
+    version="0.1.0",  # Following https://semver.org/
+    description="A Python package to read ODIM HDF5 files",
+    author="Nicolas Noé - INBO",
+    author_email="nicolas.noe@inbo.be",
+    packages=["odim_h5"],
+    python_requires=">3.7.0",
+    install_requires=[  # Installation dependencies, let's keep development dependencies in requirements.txt
+        "h5py>=3.1.0",
+        "pytz",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
